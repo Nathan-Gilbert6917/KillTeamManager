a118676dd2f253d6e158e30186dce8d94f864e55
Nathan Gilbert <weneedmoreminerals@gmail.com>
2022-07-23 11:41:57 -0400

    Connection to DB

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewURLParser: true });
+    console.log('Connected to MongoDB...');
+  } catch (error) {
+    console.error(error.message);
+    // Exit upon error
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
