f447d859021a148d4033fe138e66eac94d65fa90
Nathan Gilbert <weneedmoreminerals@gmail.com>
2022-09-14 20:04:39 -0400

    Responsive Dashboard

--- client/src/Views/Dashboard/Dashboard.jsx
+++ client/src/Views/Dashboard/Dashboard.jsx
@@ -2,27 +2,28 @@ import React, { Fragment } from "react";
 import { Navigate, Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { logout } from "../../Actions/auth";
+
 import Alert from "../../Components/Alert/Alert";
 import Button from "../../Components/Button/Button";
 import "./Dashboard.css";
 
-const Dashboard = ({ isAuthenticated }) => {
+const Dashboard = ({ auth: { isAuthenticated }, logout }) => {
   return (
     <Fragment>
       {!isAuthenticated && <Navigate replace to="/" />}
-
+      <span className="user">
+        <a href="/" onClick={logout}>
+          Logout
+        </a>
+      </span>
       <section className="page">
         <Alert />
+
         <div className="home-layout">
           <span className="title">KillTeam Manager</span>
 
           <grid className="dashboard-container">
-            <Link to="/create" className="link">
-              <Button label="Create Game" />
-            </Link>
-            <Link to="/join" className="link">
-              <Button label="Join Game" />
-            </Link>
             <Link to="/collections" className="link">
               <Button label="Collections" />
             </Link>
@@ -38,10 +39,13 @@ const Dashboard = ({ isAuthenticated }) => {
 
 Dashboard.propTypes = {
   isAuthenticated: PropTypes.bool,
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
+  auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(Dashboard);
+export default connect(mapStateToProps, { logout })(Dashboard);
